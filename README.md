2cdac9b8a98a773c3de69d35750c50bd6b1f779a
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-08-26 23:41:45 -0700

    literally made the app

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Liri-Node-App
 LIRI is a Language Interpretation and Recognition Interface. This is a command line node app that takes in parameters and gives you back data from three separate APIs -- OMDB, BandsinTown, and Spotify.
+
+# 
+*Clearly state the problem the app is trying to solve (i.e. what is it doing and why):*
+    This app is designed to allow users to search for and retrieve information on their favorie bands, songs, and movies 
+
+Give a high-level overview of how the app is organized
+    The app is organized into multiple js files (modularized). The bulk of the app is in the liri.js file, which contains the functions that make the axios calls and return values based on user input. The 
+
+
+Give start-to-finish instructions on how to run the app
+    The user opens the terminal in the correct filepath (the liri.js file), and from there they can type in node liri.js, followed by one of four commands:
+
+concert-this "band/artist name": this will return a list of concert dates/locations for the input band/artist
+
+spotify-this-song "song name": this willreturn the song artist, name, snippet, and album from spotify
+
+movie-this "movie name": this will return movie information on a specific movie to the user (title, year, imdb/rt rating, country produced, language, plot, and actors)
+
+do-what-it-says: this will run the spotify search for "I want it that way" 
+
+Include screenshots, gifs or videos of the app functioning
+
+
+
+
+Contain a link to a deployed version of the app
+
+
+Clearly list the technologies used in the app
+Axios -- method of getting/posting api calls using https
+Javascript -- language used to code
+node.Js -- command line interface where code runs
+Spotify APi -- returns a large amount of song data
+OMDB API -- returns a large amount of movie data
+Bands in Town API -- returns detailed info on various bands/artists concert dates
+
+State your role in the app development
+I developed it
---
+++ liri.js
@@ -0,0 +1,126 @@
+require("dotenv").config();
+
+// Packages
+var fs = require("fs");
+var keys = require("./keys.js")
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify)
+
+// User Input
+var userChoice = process.argv[2];
+var userQuery = process.argv[3];
+
+for (var i = 4; i < process.argv.length; i++) {
+    if (i > 4 && i < process.argv.length) {
+        userQuery += "+" + process.argv[i]; 
+    }
+    else {
+        userQuery += process.argv[i];
+    }
+}
+
+switch (userChoice) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movies();
+        break;
+    case "do-what-it-says":
+        doThis();
+        break;
+    default:
+        logThis("Please enter a valid search term, such as {concert-this},");
+        logThis("{spotify-this-song}, {movie-this}, or {do-what-it-says}");
+        break;
+}
+
+// Spotify Function
+function spotifyThis() {
+
+    // Catch empty input
+    if (!userQuery) {
+        userQuery = "The Sign Ace of Base";
+    }
+
+    spotify.search({type: "track", query: userQuery}, function(err, data) {
+        if (err) {
+            logThis(err);
+        }
+
+        var userSong = data.tracks.items;
+        logThis("Artist: " + userSong[0].artists[0].name);
+        logThis("Song Name: " + userSong[0].name);
+        logThis("Preview Link: " + userSong[0].preview_url);
+        logThis("Album: " + userSong[0].album.name);
+    });
+};
+
+function movies() {
+
+    if (!userQuery) {
+        userQuery = "Mr. Nobody";
+        logThis("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
+        logThis("It's on Netflix!");
+    }
+    
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=" + keys.movies.id)
+    .then(function(response) {
+
+        logThis("Title: " + response.data.Title);
+        logThis("Year Released: " + response.data.Year);
+        logThis("IMDB rating: " + response.data.imdbRating);
+        logThis("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        logThis("Country/Countries Produced: " + response.data.Country);
+        logThis("Language: " + response.data.Language);
+        logThis("Plot: " + response.data.Plot);
+        logThis("Cast: " + response.data.Actors);
+    });
+};
+
+function concertThis() {
+
+    if (!userQuery) {
+        userQuery = "Slayer";
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + keys.bands.id)
+    .then(function(response) {
+        for (var i = 0; i < response.data.length; i++) {
+
+            logThis("Venue Name: "+ response.data[i].venue.name);
+            logThis("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+            logThis("Date of the Event: " + moment(response.data[i].datetime).format("L"));
+        }
+    });
+}
+
+function doThis () {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+
+        if (err) {
+            logThis(err);
+        }
+
+        var readArray = data.split(",");
+
+        userQuery = readArray[1];
+
+        spotifyThis(userQuery);
+    })
+};
+
+function logThis (logQuery) {
+
+    console.log(logQuery);
+
+    fs.appendFile("log.txt", logQuery, function(err) {
+        if (err) {
+            return logThis("Error: " + err);
+        }
+    });
+};
